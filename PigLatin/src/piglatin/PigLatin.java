f75b5a5b83103a5485cf91e0229a571b18144c7e
khalidos32 <khalidos321@gmail.com>
2016-05-02 23:12:59 +0000

    day_1

---
+++ PigLatin/src/piglatin/PigLatin.java
@@ -0,0 +1,23 @@
+
+package piglatin;
+
+import java.io.*;
+
+/**
+ *
+ * @author khalido32
+ */
+public class PigLatin {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.lang.Exception
+     */
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String voyelle="";
+        String chaine=br.readLine();
+         
+    }
+    
+}
